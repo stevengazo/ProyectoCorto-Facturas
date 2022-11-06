c7c4e83887b19d5aa4996317e22f880972ace2a9
Steven Gazo M <steven.gazo@hotmail.com>
2022-11-06 16:32:24 -0600

    btnclose

--- Interfaz/Form1.vb
+++ Interfaz/Form1.vb
@@ -297,4 +297,8 @@ Public Class Form1
             MessageBox.Show(ex.Message, "Error interno", MessageBoxButtons.OK, MessageBoxIcon.Error)
         End Try
     End Sub
+
+    Private Sub SalirToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SalirToolStripMenuItem.Click
+        Close()
+    End Sub
 End Class
